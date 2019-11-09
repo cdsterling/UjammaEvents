b4abd0b922b76604ccb08eff02c411f4f85a11fd
Chad Sterling <cdsterling@gmail.com>
2019-11-09 11:35:24 -0800

    PHASE2: refactored build.py to loop through a list of pages

--- build.py
+++ build.py
@@ -1,24 +1,66 @@
 # Note this file originated from ./scriptArchive/build_advanced2.py
 
+
+pages = [
+  {
+    "content" : "content/index.html",
+    "output" : "docs/index.html",
+    "PAGE_TITLE" : "WELCOME",
+    "ACTIVE_INDEX" : "active",
+    "ACTIVE_SPACES" : "",
+    "ACTIVE_EVENTS" : "",
+    "ACTIVE_ABOUT" : "",
+  },
+  {
+    "content" : "content/spaces.html",
+    "output" : "docs/spaces.html",
+    "PAGE_TITLE" : "SPACES",
+    "ACTIVE_INDEX" : "",
+    "ACTIVE_SPACES" : "active",
+    "ACTIVE_EVENTS" : "",
+    "ACTIVE_ABOUT" : "",
+  },
+  {
+    "content" : "content/events.html",
+    "output" : "docs/events.html",
+    "PAGE_TITLE" : "EVENTS",
+    "ACTIVE_INDEX" : "",
+    "ACTIVE_SPACES" : "",
+    "ACTIVE_EVENTS" : "active",
+    "ACTIVE_ABOUT" : "",
+  },
+  {
+    "content" : "content/about.html",
+    "output" : "docs/about.html",
+    "PAGE_TITLE" : "ABOUT",
+    "ACTIVE_INDEX" : "",
+    "ACTIVE_SPACES" : "",
+    "ACTIVE_EVENTS" : "",
+    "ACTIVE_ABOUT" : "active",
+  },
+]
+
 def main():
   whole_page_template = open('templates/whole_page_template.html').read()
-  index_content = open('content/index.html').read()
-  about_content = open('content/about.html').read()
-  events_content = open('content/events.html').read()
-  spaces_content = open('content/spaces.html').read()
-
   from string import Template
   template = Template(whole_page_template)
-  index_page=template.safe_substitute(PAGE_TITLE="WELCOME", ACTIVE_INDEX="active", ACTIVE_SPACES="", ACTIVE_EVENTS="", ACTIVE_ABOUT="", PAGE_CONTENT=index_content)
-  spaces_page=template.safe_substitute(PAGE_TITLE="SPACES", ACTIVE_INDEX="", ACTIVE_SPACES="active", ACTIVE_EVENTS="", ACTIVE_ABOUT="", PAGE_CONTENT=spaces_content)
-  events_page=template.safe_substitute(PAGE_TITLE="EVENTS", ACTIVE_INDEX="", ACTIVE_SPACES="", ACTIVE_EVENTS="active", ACTIVE_ABOUT="", PAGE_CONTENT=events_content)
-  about_page=template.safe_substitute(PAGE_TITLE="ABOUT", ACTIVE_INDEX="", ACTIVE_SPACES="", ACTIVE_EVENTS="", ACTIVE_ABOUT="active", PAGE_CONTENT=about_content)
 
-  open('docs/index.html', 'w+').write(index_page)
-  open('docs/about.html', 'w+').write(about_page)
-  open('docs/events.html', 'w+').write(events_page)
-  open('docs/spaces.html', 'w+').write(spaces_page)
+  for page in pages:
+    print("creating:", page["PAGE_TITLE"])
+    print("----> Taking this content:",page["content"])
+    page_content = open(page["content"]).read()
+    full_page = template.safe_substitute(
+      PAGE_TITLE=page["PAGE_TITLE"],
+      ACTIVE_INDEX=page["ACTIVE_INDEX"],
+      ACTIVE_SPACES=page["ACTIVE_INDEX"],
+      ACTIVE_EVENTS=page["ACTIVE_INDEX"],
+      ACTIVE_ABOUT=page["ACTIVE_INDEX"],
+      PAGE_CONTENT=page_content 
+    )
+    print("----> Writing the output to", page["output"])
+    open(page["output"], 'w+').write(full_page)
+    print("------------------------")
 
-
+  
 if __name__ == "__main__":
   main()
