65a8ee911314a1df4df06323731ffbd78e83ac3e
Chad Sterling <cdsterling@gmail.com>
2019-11-19 07:32:21 -0800

    refactored main function in manage.py so that it calls a new function in utils called build_site. it felt like my main was too long and it was easy to move a chunk of the body into a new function

--- utils.py
+++ utils.py
@@ -226,3 +226,27 @@ def build_pages_list(all_content_dict):
     all_content_dict.append(page_dict)
     print(all_content_dict)
       
+def build_site(pages, fullpage_template, event_template, space_template):
+  for page in pages:
+    item_content = ""
+    full_page = None
+    page_title = page["PAGE_TITLE"]
+    #special caveat for spaces and events
+    if page_title == "spaces" or page_title == "events" :
+      if page_title == "events":
+        for event in events:
+          #for events we use the apply_event_template function to build up the content
+          item_content += ' '+ apply_event_template(event_template, event)
+      elif page_title == "spaces":
+        for space in spaces:
+          #for spaces we use the apply_space_template to build up the content
+          #note this function also builds out the individual space pages
+          item_content += apply_space_template(space_template, space, fullpage_template, page, pages)
+      
+      full_item_template = set_template(page["content"])
+      item_content =  apply_all_template(full_item_template, item_content)
+      full_page = apply_fullpage_template(fullpage_template, page, item_content, pages, False)
+      write_file(full_page, page["output"])
+    else:
+      full_page = apply_fullpage_template(fullpage_template, page, page["content"], pages)
+      write_file(full_page, page["output"])
