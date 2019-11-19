2d670cc136467b4f8262da971629b0fed20390b0
Chad Sterling <cdsterling@gmail.com>
2019-11-19 07:54:21 -0800

    updated main in manage.py to read input from command line, and process 3 command options - build (does the original work), new (doesn't do anything yet), quit (quits without doing anything

--- manage.py
+++ manage.py
@@ -1,17 +1,36 @@
 import utils
+import sys
 
 
 
 
 # Main
 def main():
-  fullpage_template = utils.set_template('templates/whole_page_template.html')
-  event_template = utils.set_template('templates/event_template.html')
-  space_template = utils.set_template('templates/space_template.html')
-  pages = []
+  command = None
 
-  utils.build_pages_list(pages)
-  utils.build_site(pages, fullpage_template, event_template, space_template)
+  if len(sys.argv) > 2:
+    command = sys.argv[1]
+
+  while True:
+    if command == "build":
+      fullpage_template = utils.set_template('templates/whole_page_template.html')
+      event_template = utils.set_template('templates/event_template.html')
+      space_template = utils.set_template('templates/space_template.html')
+      pages = []
+
+      utils.build_pages_list(pages)
+      utils.build_site(pages, fullpage_template, event_template, space_template)
+      break
+    elif command == "new":
+      utils.create_new_content()
+      break
+    elif command == "quit":
+      print("quitting without doing anything")
+      break
+    else:
+      print("invalid or missing command")
+      print("please enter either build, new or quit")
+      command = input(">> ")
       
   
 if __name__ == "__main__":
